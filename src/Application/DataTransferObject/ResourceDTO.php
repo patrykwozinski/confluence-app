18e0a8b76061950262c80492b0e15cc7e6fb7276
Patryk Woziński <patryk.wozinski@gmail.com>
2018-07-01 21:38:28 +0200

    DTO?

---
+++ src/Application/DataTransferObject/ResourceDTO.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: freeq
+ * Date: 01.07.2018
+ * Time: 21:35
+ */
+declare(strict_types=1);
+
+namespace App\Application\DataTransferObject;
+
+
+final class ResourceDTO
+{
+    /** @var int */
+    private $identity;
+
+    /** @var string */
+    private $name;
+
+    public function __construct(int $identity, string $name)
+    {
+        $this->identity = $identity;
+        $this->name = $name;
+    }
+
+    public function getIdentity(): int
+    {
+        return $this->identity;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
